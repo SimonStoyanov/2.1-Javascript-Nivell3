0ccfb00fe942a60dbf8f627a0a1dd66625933f68
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2025-03-05 19:55:35 +0100

    ex 2.2 completed

--- src/js/ex2.js
+++ src/js/ex2.js
@@ -14,3 +14,23 @@ const handleDriveInput = () => {
   const _age = document.getElementById("age").value;
   canDrive(_age);
 }
+
+// 2.2
+const isBigger = (valueA, valueB) => {
+  if (valueA > valueB) {
+    document.getElementById("compare").innerHTML = `${valueA} > ${valueB}`;
+  }
+  else if (valueA < valueB) {
+    document.getElementById("compare").innerHTML = `${valueB} > ${valueA}`;
+  }
+  else {
+    document.getElementById("compare").innerHTML = `${valueA} == ${valueB}`;
+  }
+}
+
+const handleComparisonInput = () => {
+  const _valueA = document.getElementById("value-a").value;
+  const _valueB = document.getElementById("value-b").value;
+
+  isBigger(_valueA, _valueB);
+}
