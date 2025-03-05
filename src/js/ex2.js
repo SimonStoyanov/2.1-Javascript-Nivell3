a9cf4191726a053fffca88a151a5ef7f85030c84
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2025-03-05 21:31:48 +0100

    ex 2.3 completed (+info)
    Also added parseFloat to values for more coherency to previous exercises

--- src/js/ex2.js
+++ src/js/ex2.js
@@ -11,26 +11,72 @@ const canDrive = (age) => {
 }
 
 const handleDriveInput = () => {
-  const _age = document.getElementById("age").value;
-  canDrive(_age);
+  const _age = parseFloat(document.getElementById("age").value);
+  
+  if (isNaN(_age)) {
+    document.getElementById("drive").innerHTML = "Invalid input, please put a number";
+  }
+  else {
+    canDrive(_age);
+  }
 }
 
 // 2.2
 const isBigger = (valueA, valueB) => {
-  if (valueA > valueB) {
-    document.getElementById("compare").innerHTML = `${valueA} > ${valueB}`;
-  }
-  else if (valueA < valueB) {
-    document.getElementById("compare").innerHTML = `${valueB} > ${valueA}`;
-  }
-  else {
-    document.getElementById("compare").innerHTML = `${valueA} == ${valueB}`;
-  }
+  var _string = valueA > valueB ? `${valueA} > ${valueB}` : 
+      valueA != valueB ? `${valueB} > ${valueA}` : `${valueA} == ${valueB}`;
+
+  document.getElementById("compare").innerHTML = _string;
 }
 
 const handleComparisonInput = () => {
-  const _valueA = document.getElementById("value-a").value;
-  const _valueB = document.getElementById("value-b").value;
+  const _valueA = parseFloat(document.getElementById("value-a").value);
+  const _valueB = parseFloat(document.getElementById("value-b").value);
 
-  isBigger(_valueA, _valueB);
+  if (isNaN(_valueA) || isNaN(_valueB)) {
+    document.getElementById("compare").innerHTML = "Invalid input, please put a number in both fields";
+  }
+  else {
+    isBigger(_valueA, _valueB);
+  }
+}
+
+// 2.3
+const checkValueSign = (value) => {
+  const _string = value > 0 ? `${value} is positive` : 
+    value < 0 ? `${value} is negative` : `${value} is == 0`;
+  
+  document.getElementById("valueSign").innerHTML = _string;
+}
+
+const handleCheck = () => {
+  const _value = parseFloat(document.getElementById("value").value);
+
+  if (isNaN(_value)) {
+    document.getElementById("valueSign").innerHTML = "Invalid input, please put a number";
+  }
+  else {
+    checkValueSign(_value);
+  }
+}
+
+
+const maxValue = (valueA, valueB, valueC) => {
+  const _string = valueA > valueB ? `${valueA} is the bigger number` :
+    valueB > valueC ? `${valueB} is the bigger number` : `${valueC} is the bigger number`;
+
+  document.getElementById("max").innerHTML = _string;
+}
+
+const handle3wayInput = () => {
+  const _valueA = parseFloat(document.getElementById("max-a").value);
+  const _valueB = parseFloat(document.getElementById("max-b").value);
+  const _valueC = parseFloat(document.getElementById("max-c").value);
+
+  if (isNaN(_valueA) || isNaN(_valueB) || isNaN(_valueC)) {
+    document.getElementById("max").innerHTML = "Invalid input, please put a number in the three fields";
+  }
+  else {
+    maxValue(_valueA, _valueB, _valueC);
+  }
 }
