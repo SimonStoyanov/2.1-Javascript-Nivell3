14f1d64752a6d0630735e1433b1404de9c3f9fa5
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2025-03-06 10:00:27 +0100

    ex 3.1 completed

--- src/js/ex3.js
+++ src/js/ex3.js
@@ -1,3 +1,21 @@
 'use strict'
 
 // 3.1
+function process(_value, callback) {
+  callback(_value);
+}
+
+function printValue(value) {
+  document.getElementById("callback-txt").innerHTML = `Value from callback function: ${value}`;
+}
+
+const handleCallback_3_1 = () => {
+  const _value = parseFloat(document.getElementById("callback-0").value);
+  
+  if (isNaN(_value)) {
+    document.getElementById("callback-txt").innerHTML = "Input a valid number";
+  }
+  else {
+    process(_value, printValue);
+  }
+}
