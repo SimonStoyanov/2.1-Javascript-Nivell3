9da1c590b5054f6a198e35167cffd6e6465be679
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2025-03-06 11:07:58 +0100

    ex 4.1 completed

--- src/js/ex4.js
+++ src/js/ex4.js
@@ -1,3 +1,17 @@
 'use strict'
 
 // 4.1
+const handleCombineInput = () => {
+    const _inputA = document.getElementById("strArray-a").value.trim();
+    const _inputB = document.getElementById("strArray-b").value.trim();
+    const _arrayA = _inputA.split(/[\s,]+/);
+    const _arrayB = _inputB.split(/[\s,]+/);
+
+    if (_inputA == "" || _inputB == "") {
+        document.getElementById("combine").innerHTML = "Invalid";
+    }
+    else {
+        const _newArray = [..._arrayA, ..._arrayB];
+        document.getElementById("combine").innerHTML = `New Array: [${_newArray}]`;
+    }
+}
