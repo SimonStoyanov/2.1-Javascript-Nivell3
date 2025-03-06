5d903f55c2daa872c1305d51961e560a68d7de36
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2025-03-06 10:10:11 +0100

    ex 3.2 completed

--- src/js/ex3.js
+++ src/js/ex3.js
@@ -19,3 +19,26 @@ const handleCallback_3_1 = () => {
     process(_value, printValue);
   }
 }
+
+// 3.2
+function calculator(_valueA, _valueB, callback) {
+  callback(_valueA, _valueB);
+}
+
+function addValues(_valueA, _valueB) {
+  const res = _valueA + _valueB;
+
+  document.getElementById("calc-result").innerHTML = `Result: ${res}`;
+}
+
+const handleCalcInput = () => {
+  const _valueA = parseFloat(document.getElementById("calc-a").value);
+  const _valueB = parseFloat(document.getElementById("calc-b").value);
+
+  if (isNaN(_valueA) || isNaN(_valueB)){
+    document.getElementById("calc-result").innerHTML = "Both fields need a valid number";
+  }
+  else {
+    calculator(_valueA, _valueB, addValues);
+  }
+}
