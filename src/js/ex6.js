43253f6a4a4f278006203241314323ddf5d4630e
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2025-03-08 12:26:09 +0100

    ex 6.2 completed

--- src/js/ex6.js
+++ src/js/ex6.js
@@ -6,8 +6,20 @@ const printForEach = () => {
 
     let str = "";
     array.forEach(element => {
-        str += element + "      ";
+        str += element + " ";
     });
 
     document.getElementById("print-forEach").innerHTML = `Print: ${str}`;
+}
+
+// 6.2
+const printForOf = () => {
+    const array = ['Anna', 'Bernat', 'Clara'];
+
+    let str = "";
+    for (const element of array) {
+        str += element + " ";
+    }
+
+    document.getElementById("print-forOf").innerHTML = `Print: ${str}`;
 }
