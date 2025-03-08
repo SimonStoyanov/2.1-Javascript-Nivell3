8664aec5c4b04f1d506597f89ee3957e5a00bbff
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2025-03-08 13:37:13 +0100

    ex 7.1 completed

--- src/js/ex7.js
+++ src/js/ex7.js
@@ -1,3 +1,18 @@
 'use strict'
 
 // 7.1
+function delayedPrint() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Hello World!");
+        }, 2000);
+    });
+}
+ 
+const pr_helloWorld = () => {
+    document.getElementById("pr-hello").innerHTML = "Waiting...";
+
+    delayedPrint().then((str) => {
+        document.getElementById("pr-hello").innerHTML = str;
+    });
+}
