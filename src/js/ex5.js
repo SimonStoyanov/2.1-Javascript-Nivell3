a532bccea87f1fee5f0cbfa79da6b76684b244b2
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2025-03-08 10:50:23 +0100

    ex 5.1 completed

--- src/js/ex5.js
+++ src/js/ex5.js
@@ -1,3 +1,11 @@
 'use strict'
 
 // 5.1
+const arrayPow = (power) => {
+    let array = [1, 2, 3, 4];
+    array = array.map(number    => {
+        return Math.pow(number, power);
+    })
+
+    document.getElementById("array-pow2").innerHTML = `The new values are: [${String(array)}]`;
+}
