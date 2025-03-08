bad22c766daa91a1ce2497ce3a08265174d1765d
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2025-03-08 11:00:37 +0100

    ex 5.2 completed

--- src/js/ex5.js
+++ src/js/ex5.js
@@ -3,9 +3,17 @@
 // 5.1
 const arrayPow = (power) => {
     let array = [1, 2, 3, 4];
-    array = array.map(number    => {
+    array = array.map(number => {
         return Math.pow(number, power);
     })
 
     document.getElementById("array-pow2").innerHTML = `The new values are: [${String(array)}]`;
+}
+
+// 5.2
+const arrayEven = () => {
+    let array = [1, 2, 3, 4];
+    array = array.filter(n => n % 2 == 0);
+
+    document.getElementById("array-even").innerHTML = `The new values are: [${String(array)}]`;
 }
