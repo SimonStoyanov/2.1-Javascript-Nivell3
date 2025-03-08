c835b876442848b966002156c33cd56dbccc9938
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2025-03-08 14:01:11 +0100

    ex 7.3 completed

--- src/js/ex7.js
+++ src/js/ex7.js
@@ -19,3 +19,28 @@ const pr_helloWorld = () => {
     delayedPrint().then((str) => {console.log(str)});
 }
 
+// 7.3
+function checkInput(input) {
+    return new Promise((resolve, reject) => {
+
+        if (input == "Hello") {
+            setTimeout(() => {
+                resolve("Hello!");
+            }, 2000);
+        }
+        else {
+            reject("Invalid input");
+        }
+    });
+}
+
+const pr_isHello = () => {
+    const _input = document.getElementById("input-hello").value;
+    const _output = document.getElementById("pr-input-hello");
+
+    _output.innerHTML = "Waiting...";
+
+    checkInput(_input)
+        .then((message) => { _output.innerHTML = `${message}` })
+        .catch((error) => { _output.innerHTML = `${error}` });
+}
